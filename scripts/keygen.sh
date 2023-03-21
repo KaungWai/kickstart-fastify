71a44fd21e4a7a53aa6d082aaa9d772601bb9e03
KaungWai <kaungwai219@gmail.com>
2023-03-21 11:57:43 +0900

    add mkdir command in keygen.sh

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-## Algorithm: RSA; Key length: 2048; Format: pem ##
+node -e "console.log('')"
+node -e "console.log('--------------------KEYGEN START--------------------')"
 node -e "console.log('Generating key pairs for jwt signing and verification')"
 node -e "console.log('Algorithm: RSA; Key length: 2048; Format: pem')"
 
+# create keys folder
+mkdir -p keys
+
 # generate private key
 openssl genrsa -out ./keys/private 2048
-node -e "console.log('rsa pirvate key generted')"
 
 # genreate public key using private key
 openssl rsa -in ./keys/private -pubout -out ./keys/public.pub
-node -e "console.log('rsa public key generted')"
+node -e "console.log('---------------------KEYGEN END---------------------')"
+node -e "console.log('')"
