bd3baa447a7cc4d82e878f04dafe0ceb5ad83aa5
KaungWai <kaungwai219@gmail.com>
2024-02-23 03:47:44 +0900

    update seed script

--- prisma/seeds/02_Product.ts
+++ prisma/seeds/02_Product.ts
@@ -1,13 +1,12 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 import productData from './data/product.json'
 
 export const seedProduct = async (prisma: PrismaClient) => {
-    const dataList: Prisma.ProductCreateInput[] = []
 
     await prisma.product.createMany({
         data: productData,
     })
 
-    return `Product: ${dataList.length}row(s) inserted.`
+    return `Product: ${productData.length}row(s) inserted.`
 }
