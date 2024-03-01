3a7642caaec054d7c4d22e83fc8146f11a371902
KaungWai <kaungwai219@gmail.com>
2024-03-02 00:03:15 +0900

    feat(test): test suite implementation #23

--- test/product/deleteProductById.test.ts
+++ test/product/deleteProductById.test.ts
@@ -1,27 +1,41 @@
 import 'module-alias/register'
 
 import { PrismaClient } from '@prisma/client'
+import { nanoid } from 'nanoid'
 import { test } from 'tap'
 
 import build from '@/app'
+import { SYS_CONSTANTS } from '@/constants/systemConstants'
 
 import { getJWtToken } from '../helper'
 
 test('deleteProductById: delete a product as a read-only user', async (t) => {
     const prisma = new PrismaClient()
 
+    // prepare data
+    const data = {
+        productId: nanoid(SYS_CONSTANTS.NANOID_LENGTH),
+        productName: 'product',
+        productDescription: 'description',
+        productPrice: 100,
+    }
+
+    // create a product
+    await prisma.product.create({
+        data,
+    })
+
     // login as read-only user
-    const readWirteJWT = await getJWtToken('USER00000001', 'password')
+    const readOnlyJWT = await getJWtToken('USER00000001', 'password')
+    t.ok(readOnlyJWT)
 
-    t.ok(readWirteJWT)
-
+    // delete the product
     const server = build({})
-    const productId = '123456789012'
     const response = await server.inject({
         method: 'DELETE',
-        url: `/product/${productId}`,
+        url: `/product/${data.productId}`,
         cookies: {
-            jwt: readWirteJWT,
+            jwt: readOnlyJWT,
         },
     })
     server.close()
@@ -32,7 +46,7 @@ test('deleteProductById: delete a product as a read-only user', async (t) => {
 
     const product = await prisma.product.findFirst({
         where: {
-            productId: productId,
+            productId: data.productId,
         },
     })
     prisma.$disconnect()
@@ -49,7 +63,7 @@ test('deleteProductById: delete a unknown product as a read-write user', async (
     t.ok(readWirteJWT)
 
     const server = build({})
-    const productId = 'xxxxxxxxxxxx'
+    const productId = nanoid(SYS_CONSTANTS.NANOID_LENGTH)
     const response = await server.inject({
         method: 'DELETE',
         url: `/product/${productId}`,
@@ -69,16 +83,27 @@ test('deleteProductById: delete a unknown product as a read-write user', async (
 test('deleteProductById: delete a product as a read-write user', async (t) => {
     const prisma = new PrismaClient()
 
+    // prepare data
+    const data = {
+        productId: nanoid(SYS_CONSTANTS.NANOID_LENGTH),
+        productName: 'product',
+        productDescription: 'description',
+        productPrice: 100,
+    }
+
+    // create a product
+    await prisma.product.create({
+        data,
+    })
+
     // login as read-write user
     const readWirteJWT = await getJWtToken('USER00000002', 'password')
-
     t.ok(readWirteJWT)
 
     const server = build({})
-    const productId = '123456789012'
     const response = await server.inject({
         method: 'DELETE',
-        url: `/product/${productId}`,
+        url: `/product/${data.productId}`,
         cookies: {
             jwt: readWirteJWT,
         },
@@ -90,7 +115,7 @@ test('deleteProductById: delete a product as a read-write user', async (t) => {
 
     const product = await prisma.product.findFirst({
         where: {
-            productId: productId,
+            productId: data.productId,
         },
     })
     prisma.$disconnect()
--- test/product/getProductById.test.ts
+++ test/product/getProductById.test.ts
@@ -1,34 +1,51 @@
 import 'module-alias/register'
 
+import { PrismaClient } from '@prisma/client'
+import { nanoid } from 'nanoid'
 import { test } from 'tap'
 
 import build from '@/app'
+import { SYS_CONSTANTS } from '@/constants/systemConstants'
 
 import { getJWtToken } from '../helper'
 
 test('getProductById: get a product as a read-only user', async (t) => {
-    // login as read-only user
-    const readWirteJWT = await getJWtToken('USER00000001', 'password')
+    const prisma = new PrismaClient()
 
-    t.ok(readWirteJWT)
+    // prepare data
+    const data = {
+        productId: nanoid(SYS_CONSTANTS.NANOID_LENGTH),
+        productName: 'product',
+        productDescription: 'description',
+        productPrice: 100,
+    }
+
+    // create a product
+    await prisma.product.create({
+        data,
+    })
+
+    // login as read-only user
+    const readOnlyJWT = await getJWtToken('USER00000001', 'password')
+
+    t.ok(readOnlyJWT)
 
     const server = build({})
-    const productId = '123456789012'
     const response = await server.inject({
         method: 'GET',
-        url: `/product/${productId}`,
+        url: `/product/${data.productId}`,
         cookies: {
-            jwt: readWirteJWT,
+            jwt: readOnlyJWT,
         },
     })
     server.close()
 
     t.ok(response)
     t.equal(response.statusCode, 200)
-    t.equal(response.json().result.productId, productId)
-    t.equal(response.json().result.productName, 'Dummy Product')
-    t.equal(response.json().result.productDescription, 'This is a dummy product')
-    t.equal(response.json().result.productPrice, 9)
+    t.equal(response.json().result.productId, data.productId)
+    t.equal(response.json().result.productName, data.productName)
+    t.equal(response.json().result.productDescription, data.productDescription)
+    t.equal(response.json().result.productPrice, data.productPrice)
 
     t.end()
 })
@@ -40,7 +57,7 @@ test('getProductById: get a unknown product as a read-write user', async (t) => 
     t.ok(readWirteJWT)
 
     const server = build({})
-    const productId = 'xxxxxxxxxxxx'
+    const productId = nanoid(SYS_CONSTANTS.NANOID_LENGTH)
     const response = await server.inject({
         method: 'GET',
         url: `/product/${productId}`,
@@ -58,16 +75,30 @@ test('getProductById: get a unknown product as a read-write user', async (t) => 
 })
 
 test('getProductById: get a product as a read-write user', async (t) => {
+    const prisma = new PrismaClient()
+
+    // prepare data
+    const data = {
+        productId: nanoid(SYS_CONSTANTS.NANOID_LENGTH),
+        productName: 'product',
+        productDescription: 'description',
+        productPrice: 100,
+    }
+
+    // create a product
+    await prisma.product.create({
+        data,
+    })
+
     // login as read-write user
     const readWirteJWT = await getJWtToken('USER00000002', 'password')
 
     t.ok(readWirteJWT)
 
     const server = build({})
-    const productId = '123456789012'
     const response = await server.inject({
         method: 'GET',
-        url: `/product/${productId}`,
+        url: `/product/${data.productId}`,
         cookies: {
             jwt: readWirteJWT,
         },
@@ -76,10 +107,10 @@ test('getProductById: get a product as a read-write user', async (t) => {
 
     t.ok(response)
     t.equal(response.statusCode, 200)
-    t.equal(response.json().result.productId, productId)
-    t.equal(response.json().result.productName, 'Dummy Product')
-    t.equal(response.json().result.productDescription, 'This is a dummy product')
-    t.equal(response.json().result.productPrice, 9)
+    t.equal(response.json().result.productId, data.productId)
+    t.equal(response.json().result.productName, data.productName)
+    t.equal(response.json().result.productDescription, data.productDescription)
+    t.equal(response.json().result.productPrice, data.productPrice)
 
     t.end()
 })
